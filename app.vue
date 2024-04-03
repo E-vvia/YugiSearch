5746cea2f9b7381eb47492b6a79efb4442cccce8
E-vvia <106263873+E-vvia@users.noreply.github.com>
2024-04-03 20:32:45 +0200

    Updated analytics

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <script setup>
-if(window){
+const config = useRuntimeConfig();
+
+useHead({
+  script: [
+    {
+      src: "https://static.cloudflareinsights.com/beacon.min.js",
+      'data-cf-beacon': JSON.stringify({ token: config.analyticsToken, spa: true }),
+      tagPosition: 'bodyClose',
+      defer: 'true'
+    }
+  ]
+});
+
+if (window) {
   window.artworkFile = await $fetch('https://artworks.ygorganization.com/manifest.json');
 }
 </script>
