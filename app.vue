4f6658fad153da7f5b98505a304a2808d4989825
E-vvia <106263873+E-vvia@users.noreply.github.com>
2024-04-03 20:44:30 +0200

    Analytics hotfix

--- app.vue
+++ app.vue
@@ -5,12 +5,12 @@ useHead({
   script: [
     {
       src: "https://static.cloudflareinsights.com/beacon.min.js",
-      'data-cf-beacon': JSON.stringify({ token: config.analyticsToken, spa: true }),
+      'data-cf-beacon': JSON.stringify({ token: config.public.analyticsToken, spa: true }),
       tagPosition: 'bodyClose',
       defer: 'true'
     }
   ]
-});
+}, { mode: 'client' });
 
 if (window) {
   window.artworkFile = await $fetch('https://artworks.ygorganization.com/manifest.json');
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,8 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
+    public: {
       analyticsToken: ''
+    }
   }
 })
