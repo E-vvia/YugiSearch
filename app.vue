f687d55a1f66788870b62140726d41dd80896a9c
E-vvia <106263873+E-vvia@users.noreply.github.com>
2024-09-03 10:13:07 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,17 +1,5 @@
 <script setup>
 const config = useRuntimeConfig();
-
-useHead({
-  script: [
-    {
-      src: "https://static.cloudflareinsights.com/beacon.min.js",
-      'data-cf-beacon': JSON.stringify({ token: config.public.analyticsToken, spa: true }),
-      tagPosition: 'bodyClose',
-      defer: 'true'
-    }
-  ]
-}, { mode: 'client' });
-
 if (window && !window.artworkFile) {
   window.artworkFile = await $fetch('https://artworks.ygoresources.com/manifest.json', {
     method: 'GET'
