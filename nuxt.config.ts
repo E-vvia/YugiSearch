eb0f76e2e64942e78b5bad4fcace08a7151a6f03
e-vvia <106263873+E-vvia@users.noreply.github.com>
2026-07-10 17:54:46 +0200

    Added TypeScript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+  },
 })
